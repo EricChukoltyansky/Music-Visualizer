258017b8ba7b4ab77d2eb283d75e02686feb24a8
EricChukoltyansky <kazohelmet@gmail.com>
2022-10-01 19:28:18 +0300

    installing solid

--- visualizer/src/App.tsx
+++ visualizer/src/App.tsx
@@ -1,14 +1,26 @@
-import type { Component } from "solid-js";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: Component = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/EricChukoltyansky/Music-Visualizer.git
-git push -u origin main
