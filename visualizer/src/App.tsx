4aa7082b82506ce749b1d22c85941205c06f558b
EricChukoltyansky <kazohelmet@gmail.com>
2022-10-01 19:08:55 +0300

    first commit

---
+++ visualizer/src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return <div></div>;
+};
+
+export default App;
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/EricChukoltyansky/Music-Visualizer.git
+git push -u origin main
